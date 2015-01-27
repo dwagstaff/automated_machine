983120ef1316b6636ce6f64d04a99d4d91b3f03b
dwagstaff <dave.l.wagstaff@gmail.com>
2015-01-27 06:15:58 -0800

    OPEN - task 4: Implemented TWI (Wire) class https://github.com/dwagstaff/automated_machine/issues/issue/4

--- ble_temp/src/Wire.cpp
+++ ble_temp/src/Wire.cpp
@@ -7,27 +7,65 @@
 
 #include "Wire.h"
 
+
 extern "C" {
 #include "twi_master.h"
+#include "app_error.h"
 }
 
+
 void Wire::begin(void) {
+	if(!twi_master_init())
+		APP_ERROR_HANDLER(err_BusNotAvailable);
 }
 
 void Wire::beginTransmission(uint8_t addr) {
+	size= cursor= 0;
+	this->addr= addr;
 }
 
 uint8_t Wire::write(uint8_t data) {
+	// Check for overflow
+	if( cursor == sizeof(data) )
+		return 0;
+	else {
+		buffer[cursor++]= data;
+		return 1;
+	}
 }
 
-uint8_t Wire::endTransmission(void) {
+uint8_t Wire::endTransmission(bool sendStop) {
+	bool status= twi_master_transfer( (addr << 1),
+								   buffer,
+								   cursor,
+								   sendStop ? TWI_ISSUE_STOP : TWI_DONT_ISSUE_STOP
+			                      );
+	return status ? err_Ok : err_Other;
 }
 
-uint8_t Wire::requestFrom(uint8_t addr, uint8_t len) {
+uint8_t Wire::requestFrom(uint8_t addr, uint8_t len, bool sendStop) {
+	// Insure data is not too large
+	if( len > sizeof(buffer) )
+		return 0;
+
+	// Issue the request
+	size= len;
+	cursor= 0;
+	bool status= twi_master_transfer( (addr << 1) | 1,
+									  buffer,
+									  len,
+									  sendStop ? TWI_ISSUE_STOP : TWI_DONT_ISSUE_STOP
+			                        );
+	return len;
 }
 
 uint8_t Wire::read(void) {
+	if( cursor >= sizeof(buffer) )
+		return 0;
+	else
+		return buffer[cursor++];
 }
 
 uint16_t Wire::available(void) {
+	return size - cursor;
 }
